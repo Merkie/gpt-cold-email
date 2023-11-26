df31f137e04db98053706e4ed812a5f31b16f0e8
Merkie <merkie@post.com>
2023-11-26 16:58:14 -0600

    add server file for generate emails dashboard

---
+++ src/routes/dash/email/generate-emails/+page.server.ts
@@ -0,0 +1,41 @@
+export const load = async ({ locals: { prisma, user } }) => {
+	const emails = await prisma.generated_emails.findMany({
+		where: {
+			user_id: user.id,
+			sent: false
+		},
+		include: {
+			employee: {
+				select: {
+					name: true,
+					business_id: true
+				}
+			}
+		}
+	});
+
+	const businessIdsWithEmails = [...new Set(emails.map((email) => email.employee.business_id))];
+
+	const businessesWithoutGeneratedEmails = await prisma.businesses.findMany({
+		where: {
+			id: {
+				notIn: businessIdsWithEmails
+			},
+			employees: {
+				some: {}
+			}
+		},
+		include: {
+			employees: {
+				select: {
+					id: true
+				}
+			}
+		}
+	});
+
+	return {
+		emails,
+		businessesWithoutGeneratedEmails
+	};
+};
