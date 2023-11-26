e7c38da8f562228666b21cd1f61b41479b88de45
Merkie <merkie@post.com>
2023-11-26 17:35:39 -0600

    connect generate emails to api

--- src/routes/dash/email/generate-emails/+page.svelte
+++ src/routes/dash/email/generate-emails/+page.svelte
@@ -1,12 +1,95 @@
 <script lang="ts">
+	import { invalidateAll } from '$app/navigation';
+	import type { businesses } from '@prisma/client';
+	import { toast } from 'svelte-sonner';
+	import semaphore from 'semaphore';
+
 	export let data;
+
+	let selectedTemplateIndex = 0;
+
+	let loading = false;
+
+	async function generateEmailFromBusiness(business: businesses) {
+		const determinationResponse = await fetch('/api/v1/email/determine-recipient', {
+			method: 'POST',
+			body: JSON.stringify({
+				business_id: business.id
+			})
+		}).then((res) => res.json());
+
+		if (!determinationResponse.employee) {
+			toast.error('Email Generation Error', {
+				class: 'display',
+				descriptionClass: 'text',
+				description: `Couldn't find an employee to send the email to for ${business.title}. Error: ${determinationResponse.error}`
+			});
+			return;
+		}
+
+		const emailGenResponse = await fetch('/api/v1/email/generate', {
+			method: 'POST',
+			body: JSON.stringify({
+				template_id: data.templates[selectedTemplateIndex].id,
+				employee_id: determinationResponse.employee.id,
+				email: determinationResponse.employee.email,
+				save: true
+			})
+		}).then((res) => res.json());
+
+		if (emailGenResponse.error) {
+			toast.error('Email Generation Error', {
+				class: 'display',
+				descriptionClass: 'text',
+				description: `Couldn't generate email for ${business.title}. Error: ${emailGenResponse.error}`
+			});
+			return;
+		}
+
+		toast.success('Email Generated', {
+			class: 'display',
+			descriptionClass: 'text',
+			description: `Successfully generated email for ${business.title}.`
+		});
+
+		await invalidateAll();
+	}
+
+	async function handleGenerateEmails() {
+		if (loading) return;
+		loading = true;
+
+		const sem = semaphore(5);
+		for (const business of data.businessesWithoutGeneratedEmails.slice(0, 5)) {
+			sem.take(async () => {
+				await generateEmailFromBusiness(business);
+				sem.leave();
+			});
+		}
+
+		loading = false;
+	}
 </script>
 
 <div class="mb-8 flex items-center gap-8 border-b p-4 pb-8">
 	<p class="display text-xl">Generate Emails</p>
-	<button class="rounded-md bg-gray-800 p-3 px-8 text-gray-50"
-		>Generate Emails ({data.businessesWithoutGeneratedEmails.length})</button
+	<button
+		disabled={loading}
+		on:click={handleGenerateEmails}
+		class="rounded-md bg-gray-800 p-3 px-8 text-gray-50"
+		>{loading
+			? 'Generating Emails...'
+			: `Generate Emails (${data.businessesWithoutGeneratedEmails.length})`}</button
 	>
+	<p>Email Template:</p>
+	<select
+		bind:value={selectedTemplateIndex}
+		class="w-[300px] rounded-md border border-gray-200 bg-white p-3 px-4"
+	>
+		{#each data.templates as template, index}
+			<option value={index}>{template.name}</option>
+		{/each}
+	</select>
 </div>
 
 {#if data.emails.length === 0}
