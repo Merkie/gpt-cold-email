8232c36775ce87c7f3e88b8770396bfd2687bc73
Merkie <merkie@post.com>
2023-11-25 22:51:02 -0600

    add route gaurds to hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,12 @@
 import prisma from '$lib/resources/prisma'
-import type { Handle } from '@sveltejs/kit'
+import { redirect, type Handle } from '@sveltejs/kit'
 import jwt from 'jsonwebtoken'
 import { JWT_SECRET } from '$env/static/private'
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.prisma = prisma
 
+	// Token decoding and user fetching
 	const token = event.cookies.get('gptce-token')
 	if (token) {
 		try {
@@ -15,8 +16,21 @@ export const handle: Handle = async ({ event, resolve }) => {
 					id: decodedToken.id,
 				},
 			})
-			if (userFromToken) event.locals.user = userFromToken
+			if (userFromToken) {
+				// @ts-expect-error
+				delete userFromToken.password
+				event.locals.user = userFromToken
+			}
 		} catch {}
+	}
+
+	const isRouteDashboard = event.url.pathname.startsWith('/dash')
+	const isRouteApiIgnoreAuth =
+		event.url.pathname.startsWith('/api/v1/') && !event.url.pathname.startsWith('/api/v1/auth')
+
+	// if the user is trying to access an authenticated rotue without being logged in, redirect to login
+	if ((isRouteApiIgnoreAuth || isRouteDashboard) && !event.locals.user) {
+		throw redirect(307, '/login')
 	}
 
 	return await resolve(event)
