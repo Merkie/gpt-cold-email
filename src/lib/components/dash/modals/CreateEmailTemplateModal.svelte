3f3ccbadb265034c11177528f519751895be9dab
Merkie <merkie@post.com>
2023-11-26 12:43:10 -0600

    add template creation logic with some extra stuff for sonner

--- src/lib/components/dash/modals/CreateEmailTemplateModal.svelte
+++ src/lib/components/dash/modals/CreateEmailTemplateModal.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { toast } from 'svelte-sonner';
 	import ModalBase from './ModalBase.svelte';
 
 	export let close: () => void;
@@ -6,6 +7,36 @@
 	let templateName: string;
 	let subject: string;
 	let body: string;
+
+	let loading = false;
+
+	async function handleTemplateCreation() {
+		if (loading) return;
+		loading = true;
+		const response = await fetch('/api/v1/email/template/create', {
+			method: 'POST',
+			body: JSON.stringify({
+				name: templateName,
+				subject,
+				body
+			})
+		}).then((res) => res.json());
+		loading = false;
+
+		if (response.template) {
+			toast.success('Template Created', {
+				class: 'display',
+				descriptionClass: 'text',
+				description: `"${response.template.name}"`
+			});
+		} else if (response.error) {
+			toast.error('Template Creation Error', {
+				class: 'display',
+				descriptionClass: 'text',
+				description: response.error
+			});
+		}
+	}
 </script>
 
 <ModalBase title={'Create Email Template'} width={600} {close}>
@@ -26,5 +57,11 @@
 		bind:value={body}
 		class="mb-4 resize-y rounded-md border border-gray-200 bg-white p-2 px-4"
 	/>
-	<button class="mt-8 rounded-md bg-gray-800 p-4 font-bold text-gray-50"> Submit </button>
+	<button
+		disabled={loading}
+		on:click={handleTemplateCreation}
+		class="mt-8 rounded-md bg-gray-800 p-4 font-bold text-gray-50"
+	>
+		{loading ? 'Loading...' : 'Submit'}
+	</button>
 </ModalBase>
