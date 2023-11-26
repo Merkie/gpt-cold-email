7eb4ab6662d5359ba923e26effc5165d4a15b7a2
Merkie <merkie@post.com>
2023-11-26 12:22:40 -0600

    create modal base component for modal reusability

--- src/lib/components/dash/modals/CreateEmailTemplateModal.svelte
+++ src/lib/components/dash/modals/CreateEmailTemplateModal.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { fade, fly } from 'svelte/transition';
+	import ModalBase from './ModalBase.svelte';
 
 	export let close: () => void;
 
@@ -8,25 +8,7 @@
 	let body: string;
 </script>
 
-<button
-	transition:fade={{ duration: 100 }}
-	on:click={(e) => {
-		if (e.target === e.currentTarget) close();
-	}}
-	class="fixed left-0 top-0 z-30 h-screen w-screen backdrop-blur-sm"
->
-</button>
-
-<div
-	transition:fly={{ y: 50, duration: 200 }}
-	class="fixed left-1/2 top-1/2 z-30 flex w-[90%] max-w-[600px] -translate-x-1/2 -translate-y-1/2 flex-col rounded-lg bg-gray-50 p-8 py-12 shadow-md"
->
-	<div class="mb-8 flex items-center justify-between">
-		<p class="text-xl font-semibold">Create Email Template</p>
-		<button on:click={close} class="p-2">
-			<i class="bi bi-x-lg"></i>
-		</button>
-	</div>
+<ModalBase title={'Create Email Template'} width={600} {close}>
 	<p class="mb-2">Template Name:</p>
 	<input
 		bind:value={templateName}
@@ -45,4 +27,4 @@
 		class="mb-4 resize-y rounded-md border border-gray-200 bg-white p-2 px-4"
 	/>
 	<button class="mt-8 rounded-md bg-gray-800 p-4 font-bold text-gray-50"> Submit </button>
-</div>
+</ModalBase>
---
+++ src/lib/components/dash/modals/ModalBase.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { fade, fly } from 'svelte/transition';
+
+	export let close: () => void;
+	export let title: string;
+	export let width = 400;
+</script>
+
+<button
+	transition:fade={{ duration: 100 }}
+	on:click={(e) => {
+		if (e.target === e.currentTarget) close();
+	}}
+	class="fixed left-0 top-0 z-30 h-screen w-screen backdrop-blur-sm"
+>
+</button>
+
+<div
+	transition:fly={{ y: 50, duration: 200 }}
+	style={`max-width: ${width}px;`}
+	class="fixed left-1/2 top-1/2 z-30 flex w-[90%] -translate-x-1/2 -translate-y-1/2 flex-col rounded-lg bg-gray-50 p-8 py-12 shadow-md"
+>
+	<div class="mb-8 flex items-center justify-between">
+		<p class="display text-xl font-semibold">{title}</p>
+		<button on:click={close} class="p-2">
+			<i class="bi bi-x-lg"></i>
+		</button>
+	</div>
+	<slot />
+</div>
