d9a0740c5648af62ffa6f8b9456f0ad0cb5940c4
Merkie <merkie@post.com>
2023-11-30 13:49:18 -0600

    add email view modal

---
+++ src/lib/components/dash/modals/ViewEmailModal.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { employees, generated_emails } from '@prisma/client';
+	import ModalBase from './ModalBase.svelte';
+
+	export let close: () => void;
+	export let email: generated_emails & { employee: employees };
+</script>
+
+<ModalBase width={600} title={`Email Details`} {close}>
+	<p class="display text-xl font-semibold">{email.subject}</p>
+	<div class="mt-4 flex items-center gap-4">
+		<p>{'To:'}</p>
+		<div class="flex items-center gap-2">
+			<p class="text-lg">{email.employee.name}</p>
+			{#each email.employee.roles || [] as role}
+				<p class="rounded-sm bg-gray-400 p-1 px-2 text-sm text-gray-50">{role}</p>
+			{/each}
+		</div>
+	</div>
+	<div class="relative mt-8 whitespace-pre-wrap rounded-sm bg-gray-100 p-4">
+		<p
+			style={`font-size: 60px`}
+			class="absolute left-[5px] top-[10px] -translate-x-1/2 -translate-y-1/2 text-gray-500 opacity-10"
+		>
+			<i class="bi bi-quote"></i>
+		</p>
+		<p>{email.body}</p>
+	</div>
+	<button on:click={close} class="mt-8 rounded-md bg-gray-800 p-4 font-bold text-gray-50">
+		Close
+	</button>
+</ModalBase>
