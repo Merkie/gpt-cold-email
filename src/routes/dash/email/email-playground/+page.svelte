b9bef0c5f9904617b9d92d731d10d2f0134416da
Merkie <merkie@post.com>
2023-11-30 09:35:29 -0600

    bug fix for playground route with no templates or businesses

--- src/routes/dash/email/email-playground/+page.svelte
+++ src/routes/dash/email/email-playground/+page.svelte
@@ -42,6 +42,7 @@
 <div class="mb-8 flex items-center gap-8 border-b p-4 pb-8">
 	<p class="display text-xl">Email Playground</p>
 </div>
+
 <div class="flex min-h-[600px] flex-col-reverse gap-4 px-4 md:flex-row">
 	<div style="flex: 2;">
 		<p class="display my-8 text-xl md:hidden">
@@ -50,14 +51,14 @@
 		</p>
 		<input
 			bind:value={generatedSubject}
-			placeholder={data.templates[selectedTemplateIndex].subject || ''}
+			placeholder={(data.templates[selectedTemplateIndex] || { subject: '' }).subject}
 			disabled
 			class="mb-4 w-full rounded-md border border-gray-200 bg-white p-2 px-4"
 			type="text"
 		/>
 		<textarea
 			bind:value={generatedBody}
-			placeholder={data.templates[selectedTemplateIndex].body || ''}
+			placeholder={(data.templates[selectedTemplateIndex] || { body: '' }).body}
 			disabled
 			class="mb-4 flex h-full w-full resize-y flex-col gap-4 rounded-md border border-gray-200 bg-white p-2 px-4"
 		></textarea>
@@ -86,7 +87,7 @@
 			class="mb-4 w-full rounded-md border border-gray-200 bg-white p-2 px-4"
 		>
 			{#each data.businesses as business, index}
-				<option value={index}>{business.title}</option>
+				<option value={index}>{business.name}</option>
 			{/each}
 		</select>
 		<p class="mb-2">Contact:</p>
@@ -94,12 +95,12 @@
 			bind:value={selectedEmployeeIndex}
 			class="mb-8 w-full rounded-md border border-gray-200 bg-white p-2 px-4"
 		>
-			{#each data.businesses[selectedBusinessIndex].employees as employee, index}
+			{#each (data.businesses[selectedBusinessIndex] || { employees: [] }).employees as employee, index}
 				<option value={index}>{employee.name}</option>
 			{/each}
 		</select>
 		<button
-			disabled={loading}
+			disabled={loading || data.businesses.length === 0 || data.templates.length === 0}
 			on:click={functionHandleGenerate}
 			class="w-full rounded-md bg-gray-800 p-4 font-bold text-gray-50"
 		>
